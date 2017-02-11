6c3f20631fd9112d4a9322c5dfecf121bbbfc962
Erik Ziegler <erik.sweed@gmail.com>
2017-02-11 10:58:28 +0100

    WIP

--- test/test.js
+++ test/test.js
@@ -1,13 +1,34 @@
+var assert = chai.assert;
 
-(function(csc) {
-    module("cornerstoneCore.generateLut");
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
 
-    test("noop test", function () {
-        // Arrange
+    assert.equal(arr.length, 0);
+  });
+});
 
-        // Act
 
-        // Assert
-        ok(true, "noop");
+describe('image', function() {
+  it('should be drawn correctly', function() {
+    var element = document.createElement('element');
+    element.style.width = '512px';
+    element.style.height = '512px';
+    element.style.color = 'red';
+
+    document.body.appendChild(element);
+
+    cornerstone.enable(element);
+    cornerstone.loadAndCacheImage('example://1').then(function(image) {
+    	cornerstone.displayImage(element, image)
+
+    	var enabledElement = cornerstone.getEnabledElement(element);
+    	var canvas = enabledElement.canvas;
+    	var context = canvas.getContext("2d");
+    	var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    	console.log(imageData);
+    	assert.equal(arr.length, 0);	
     });
+  });
 });
